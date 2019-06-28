cb1bd0da77d242971389285750141cfb61b1c740
jiangxuewen <445251692@qq.com>
2019-06-28 14:48:57 +0800

    约定消息队列的传输结构

--- src/MqSendDataStruct.php
+++ src/MqSendDataStruct.php
@@ -4,31 +4,7 @@
 namespace hq\mq;
 
 
-class MqSendDataStruct
+abstract class MqSendDataStruct
 {
-    //用于无法定义结构的未知结构数据
-    private $_data = '';
-
-    public function toArray(): array
-    {
-        return get_object_vars($this);
-    }
-
-    /**
-     * @return string
-     */
-    final public function getData(): string
-    {
-        return $this->_data;
-    }
-
-    /**
-     * @param string $data
-     */
-    final public function setData(string $data): void
-    {
-        $this->_data = $data;
-    }
-
-
+    abstract public function toArray(): array;
 }
